5561f708861f63b5a77fc72dc71d6395b63e7168
Nicholas Reeder <nickreeder32@gmail.com>
2021-06-04 09:45:50 -0700

    :wrench: add home controller

--- PierresBakery/Controllers/HomeController.cs
+++ PierresBakery/Controllers/HomeController.cs
@@ -11,6 +11,21 @@ namespace PierresBakery.Controllers
 {
     public class HomeController : Controller
     {
-        
+        private readonly PierresBakeryContext _db;
+        public HomeController(PierresBakeryContext db)
+        {
+            _db = db;
+        }
+
+    public ActionResult Index()
+{
+      var model = new Dictionary<string, object>();
+      List<Flavor> flavors = _db.Flavors.ToList();
+      List<Treat> treats = _db.Treats.ToList();
+      model.Add("treats", treats);
+      model.Add("flavors", flavors);
+      return View(model);
+}
+            
     }
 }
